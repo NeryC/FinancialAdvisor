534c330e7631b0e580a3c320f4621829f1a83130
NeryC <nalbertoc132@gmail.com>
2021-01-08 02:07:22 -0300

    initial config

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+const mainContainer = document.getElementById('app');
+
+ReactDOM.render(<App />, mainContainer);
